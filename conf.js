34769a8732607e6c40dfacee66b49ce9c5b69564
Czabator <filip.czabator@capgemini.com>
2016-07-13 16:23:57 +0200

    spec fix and adding logger

--- conf.js
+++ conf.js
@@ -1,6 +1,6 @@
 exports.config = {
   seleniumAddress: 'http://localhost:4444/wd/hub',
-  specs: ['tests/todomvc/test.spec.js'],
+  specs: ['tests/todomvc/main.spec.js'],
   jasmineNodeOpts: {
     showColors: true
   },
--- tests/todomvc/main.spec.js
+++ tests/todomvc/main.spec.js
@@ -1,8 +1,11 @@
-import Homepage from '../../page-objects/todomvc/homepageClass.po.js';
+import Homepage from '../../page-objects/todomvc/homepage.po.js';
+import log4js from 'log4js';
 
 describe('todomvc tests', function() {
+  let logger = log4js.getLogger();
   var homepage = new Homepage();
   homepage.get();
+  logger.info('test log info');
   it('shouldMatchTitle', function() {
     expect(homepage.getTitle().getText()).toEqual('todos');
   });
